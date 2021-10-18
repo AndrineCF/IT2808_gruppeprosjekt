0bfe1fa08c0fc4416e60b5189d092bcab961ab0c
Dumbr-status <69890492+Dumbr-status@users.noreply.github.com>
2021-10-18 21:56:00 +0200

    Navbar forbedret kraftig

--- prosjekt/js/navbar.js
+++ prosjekt/js/navbar.js
@@ -4,9 +4,11 @@ const nav = document.getElementById('dropdown');
 /* code changed from: https://stackoverflow.com/questions/41880822/have-multiple-click-to-open-dropdown-menus-only-want-one-open-at-a-time */
 /* function that uses the id inserted so that the button can display th dropdowncontent associated with it */
 function overNavbar(id) {
-         document.getElementById(id).classList.toggle("show");
+        document.getElementById(id).classList.add("show");
+        console.log("over")
 }
 
 function leaveNavbar(id) {
         document.getElementById(id).classList.remove("show");
+        console.log("leave")
 }
