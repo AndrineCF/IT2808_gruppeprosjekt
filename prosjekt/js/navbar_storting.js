2b75debfb2914fee759e695682a6b8d3de1f9ea8
Andrine Celine Flatby <andrine.celine.flatby@gmail.com>
2021-10-18 16:36:53 +0200

    har latt til sturktur til navbar til stortering, jobbes med å legge til et javgascript for enkler bruk

--- prosjekt/js/navbar_storting.js
+++ prosjekt/js/navbar_storting.js
@@ -1,3 +1,15 @@
-let kjonnEl = document.getElementById("kjonn")
-let alderEl = document.getElementById("alder")
+const contentBodyEl = document.getElementById("content-body")
+let sectionEl = document.createElement("section")
+let navbarEl = document.createElement("div")
 
+navbarEl.id = "navbar-storting"    
+
+function createSelectMenny(){
+    let labelEl = document.createElement("label")
+    labelEl.setAttribute("for", "kjonn")
+}
+
+
+
+sectionEl.append(navbarEl)
+contentBodyEl.append(sectionEl)
