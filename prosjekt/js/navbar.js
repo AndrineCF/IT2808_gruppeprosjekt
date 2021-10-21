1eb538418e491b39ca3771ff7a7f4533babcc5eb
Dumbr-status <69890492+Dumbr-status@users.noreply.github.com>
2021-10-21 20:58:14 +0200

    started translating to js

--- prosjekt/js/navbar.js
+++ prosjekt/js/navbar.js
@@ -1,7 +1,24 @@
-/* links to navbar */
-const nav = document.getElementById('dropdown');
 
-/* code changed from: https://stackoverflow.com/questions/41880822/have-multiple-click-to-open-dropdown-menus-only-want-one-open-at-a-time */
+/* links to header */
+var nav = document.getElementsByClassName("navbar")[0];
+
+// create the a element
+var a = document.createElement("a");
+
+// create img element
+var img = document.createElement("img");
+
+// link img to headerLogo
+img.src = "img/headerLogo.png";
+
+// adds the img to the a
+a.appendChild(img);
+
+// adds the hyperlink to a
+a.href = "forside.html"
+
+nav.appendChild(a);
+
 /* function that uses the id inserted so that the button can display th dropdowncontent associated with it */
 function overNavbar(id) {
         document.getElementById(id).classList.add("show");
