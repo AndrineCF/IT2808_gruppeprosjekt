5f45f1b8d3c6a84307d79fbcc33a667865f49ada
Dumbr-status <69890492+Dumbr-status@users.noreply.github.com>
2021-10-22 18:37:52 +0200

    navbar complete!
    Mangler kanskje noe styling, men alt er oversatt til js

--- prosjekt/js/navbar.js
+++ prosjekt/js/navbar.js
@@ -50,6 +50,82 @@ a.href = "fosterhjem.html"
 // adds a to the navbar
 nav.appendChild(a);
 
+// create the div element that shall contain the dropdown
+var dropdownDiv = document.createElement("div");
+
+// gives the dropdown the class dropdown2
+dropdownDiv.classList = "dropdown2";
+
+// create the div element that shall contain the dropdownbtn and dropdown itself
+var dropdownBtn = document.createElement("button");
+
+// gives the dropdown the class dropdown2
+dropdownBtn.classList = "dropbtn2";
+
+dropdownBtn.innerHTML = "Adopsjon";
+
+// adds dropdownBtn to dropdownDiv
+dropdownDiv.appendChild(dropdownBtn);
+
+// create the div element that shall contain the dropdown
+var dropdownContent = document.createElement("div");
+
+dropdownContent.id = "adopsjon";
+
+// gives the dropdown the class dropdown2
+dropdownContent.classList = "dropdown2-content";
+
+// reset var a 
+a = document.createElement("a");
+
+a.innerHTML = "om adopsjon";
+// adds the hyperlink to a
+a.href = "adopsjon.html"
+
+dropdownContent.appendChild(a);
+
+// reset var a 
+a = document.createElement("a");
+
+a.innerHTML = "katt";
+// adds the hyperlink to a
+a.href = "om_katt.html"
+
+dropdownContent.appendChild(a);
+
+// reset var a 
+a = document.createElement("a");
+
+a.innerHTML = "hund";
+// adds the hyperlink to a
+a.href = "om_hund.html"
+
+dropdownContent.appendChild(a);
+
+// reset var a 
+a = document.createElement("a");
+
+a.innerHTML = "skjema";
+// adds the hyperlink to a
+a.href = "kontakt_skjema.html"
+
+dropdownContent.appendChild(a);
+
+
+// adds dropdownContainer to dropdownDiv
+dropdownDiv.appendChild(dropdownContent);
+
+// adds a to the navbar
+nav.appendChild(dropdownDiv);
+
+// adds mouseover to the button so that the content can be shown
+dropdownBtn.onmouseover = function(){
+        overNavbar("adopsjon")
+};
+
+dropdownDiv.onmouseleave = function(){
+        leaveNavbar("adopsjon")
+};
 
 
 /* function that uses the id inserted so that the button can display th dropdowncontent associated with it */
