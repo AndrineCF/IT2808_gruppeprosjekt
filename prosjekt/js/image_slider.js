1b88291f806117534cd60288ebf416f92c09ee84
Sunniva-andresen <86493830+Sunniva-andresen@users.noreply.github.com>
2021-10-24 22:22:56 +0200

    feat: startet på adopsjons side

--- prosjekt/js/image_slider.js
+++ prosjekt/js/image_slider.js
@@ -1,9 +1,11 @@
 ;(function() {
-    window.ImageSlider = function({ folder = '', images = [] } = {}) {
+    window.ImageSlider = function({ folder = '', images = [], pathPrefix = '' } = {}) {
         const image = document.querySelector('.gallery-images > img')
         const [previous, next] = Array.from(document.querySelectorAll('.gallery-toggles > button'))
     
         let currentImageIndex = 0
+        let imageRef = null
+        let buttonRef = null
 
         function setSrcOnImage(index) {
             currentImageIndex = index
@@ -11,18 +13,42 @@
             previous.style.visibility = index === 0 ? 'hidden' : ''
             next.style.visibility = index === images.length - 1 ? 'hidden' : ''
 
-            image.setAttribute('src', `/images/${folder}/${images[index]}`)
+            image.setAttribute('src', `${pathPrefix}/images/${folder}/${images[index]}`)
+            
+            if (imageRef) {
+                imageRef.setAttribute('src', `${pathPrefix}/images/${folder}/${images[index]}`)
+            }
         }
 
         setSrcOnImage(currentImageIndex)
     
-        next.onclick = function() {
+        next.onclick = function(e) {
+            e.stopPropagation()
             setSrcOnImage(Math.min(images.length - 1, currentImageIndex + 1))
         }
         
-        previous.onclick = function() {
+        previous.onclick = function(e) {
+            e.stopPropagation()
             setSrcOnImage(Math.max(0, currentImageIndex - 1))
         }
+
+        window.addEventListener('keydown', (e) => {
+            console.log(e.key)
+            if (e.key.toLowerCase() === 'arrowleft') {
+                e.preventDefault()
+                previous.click()
+            }
+
+            if (e.key.toLowerCase() === 'arrowright') {
+                e.preventDefault()
+                next.click()    
+            }
+
+            if (e.key.toLowerCase() === 'escape' && imageRef !== null) {
+                e.preventDefault()
+                buttonRef && buttonRef.click()
+            }
+        })
 
         
         image.onclick = function() {
@@ -34,33 +60,30 @@
             const button = document.createElement('button')
             const clonedImage = image.cloneNode(true)
 
+            imageRef = clonedImage
+            buttonRef = button
+
             const closeModal = function(e) {
                 const element = document.querySelector('.gallery-modal')
 
                 if (!element) {
                     return    
                 }
-    
-                let target = e.target
 
-                if (target === image) {
+                if (e.target === image) {
                     return
                 }
-    
-                while (target && target !== document.body) {
-                    if (target === clonedImage) {
-                        return
-                    }
-                    
-                    target = target.parentElement
-                }
-                
+
+                imageRef = null
+                buttonRef = null
                 document.body.removeChild(element)
             }
                 
             button.onclick = function closeModal() {
                 document.body.removeChild(modal)
                 window.removeEventListener('click', closeModal)
+                imageRef = null
+                buttonRef = null
             }
 
             window.addEventListener('click', closeModal)
