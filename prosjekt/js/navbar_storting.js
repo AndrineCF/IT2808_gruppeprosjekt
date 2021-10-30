bdb631b80c7582eae280be8138c3b43f7a72e122
Andrine Celine Flatby <andrine.celine.flatby@gmail.com>
2021-10-30 19:00:09 +0200

    fikset på kjonn og alder meny sorting bar

--- prosjekt/js/navbar_storting.js
+++ prosjekt/js/navbar_storting.js
@@ -15,13 +15,13 @@ navbarEl.id = "navbar-storting"
 var jsonData = {
     "selectMeny": {
         "hund": {
-            "kjonn" : ["Hun", "Han"],
-            "alder" : [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
+            "kjonn" : ["Hun", "Han", "begge"],
+            "alder" : ["1-5","6-10","11-13", "alle"]
         },
 
         "katt": {
-            "kjonn" : ["Hun", "Han"],
-            "alder" : [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+            "kjonn" : ["Hun", "Han", "begge"],
+            "alder" : ["1-5","6-10","11-15","16-20", "alle"]
         }
     },
 
